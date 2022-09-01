f52c5788146d2739231da54f34c63a83f847d05f
Xander Cabatic <Eminent.Mass@gmail.com>
2022-09-01 14:51:05 -0700

    Refactored OrderManager to use a separate class for its lookup table. This is more concise.

--- src/main/java/orderbook/orderbook/OrderManager.kt
+++ src/main/java/orderbook/orderbook/OrderManager.kt
@@ -6,31 +6,40 @@ import org.bukkit.scheduler.BukkitRunnable
 import java.util.*
 import kotlin.collections.HashMap
 
-// Multimap utilities
-private fun<K, V> MutableMap<K, MutableList<V>>.removeSingle(key: K, value: V) {
+private class OrderLookupTable(private var table: MutableMap<Trade, MutableList<UUID>>) {
+    constructor(list: List<Order>) : this(
+        list.filter { it.stage.isPosted }
+        .groupByTo( HashMap(), { o -> o.trade }, { o -> o.id })
+    )
 
-    // remove element value in list at key
-    // once it is removed if the list is empty remove it as well.
-    this[key]?.remove(value)
+    fun remove(key: Trade, value: UUID) {
+        // remove element value in list at key
+        // once it is removed if the list is empty remove it as well.
+        table[key]?.remove(value)
 
-    if( this[key]?.isEmpty() == true ){
-        this.remove(key)
+        if( table[key]?.isEmpty() == true ){
+            table.remove(key)
+        }
     }
-}
-private fun<K, V> MutableMap<K, MutableList<V>>.putSingle(key: K, value: V) {
-    // if a list already exists at key, add value to it otherwise
-    // create a new list with element value and add it at key.
-    this[key]?.add(value) ?: run {
-        this[key] = mutableListOf(value)
+
+    fun put(key: Trade, value: UUID) {
+        // if a list already exists at key, add value to it otherwise
+        // create a new list with element value and add it at key.
+        table[key]?.add(value) ?: run {
+            table[key] = mutableListOf(value)
+        }
     }
+
+    fun get(key: Trade): MutableList<UUID>? = table[key]
+
+    fun getInverse(key: Trade): MutableList<UUID>? = get(key.inverse)
 }
 
-class Saver(private var lastSaveHash: Int = 0) : BukkitRunnable() {
+class Saver(private var plugin: OrderBook, private var lastSaveHash: Int = 0) : BukkitRunnable() {
     override fun run() {
 
         // only save if the state has changed
-        val instance = OrderBook.instance ?: return
-        val manager = instance.orderManager ?: return
+        val manager = plugin.orderManager ?: return
         if (manager.hashCode() == lastSaveHash) return
 
         try {
@@ -38,42 +47,38 @@ class Saver(private var lastSaveHash: Int = 0) : BukkitRunnable() {
             lastSaveHash = manager.hashCode()
         } catch (e: Exception) {
             e.printStackTrace()
-            val logger = instance.logger
+            val logger = plugin.logger
             logger.warning(e.message)
             logger.warning("Failed to save order book state. Save attempt scheduled in 10 seconds")
 
             // Possibly not blocked in 10 seconds
-            this.runTaskLater(instance, (10 * 20).toLong())
+            this.runTaskLater(plugin, (10 * 20).toLong())
         }
     }
 }
 
-class OrderManager(list: List<Order> = emptyList()) {
+class OrderManager(plugin: OrderBook, list: List<Order> = emptyList()) {
     private val orders: MutableMap<UUID, Order>
-    private val openTrades: HashMap<Trade, MutableList<UUID>>
+    private val openTrades: OrderLookupTable
+    private val saver: Saver
 
     init {
         orders = list.associateBy { it.id }.toMutableMap()
-        openTrades = list.filter { it.stage.isPosted }
-            .groupByTo( HashMap(), { o -> o.trade }, { o -> o.id })
+        openTrades = OrderLookupTable(list)
 
         // register saving
         val period = (30 * 60 * 20).toLong() // 30 minutes * 60 seconds/minute * 20 ticks/second
-        Saver().runTaskTimer(OrderBook.instance!!, period, period)
+        saver = Saver(plugin)
+        saver.runTaskTimer(plugin, period, period)
     }
 
     fun getOrder(id: UUID): Order? = orders[id]
 
     fun removeOrder(id: UUID): Boolean {
         val (_, _, trade) = orders.remove(id) ?: return false
-        openTrades[trade]?.remove(id)
+        openTrades.remove(trade, id)
         return true
     }
-
-    /*
-    val orderMap: MutableMap<UUID, Order>
-        get() = orders
-     */
 
     fun getOrders(): MutableCollection<Order> = orders.values
 
@@ -88,7 +93,7 @@ class OrderManager(list: List<Order> = emptyList()) {
     fun postOrder(trade: Trade): Order {
         val order = trade.toNewOrder()
         orders[order.id] = order
-        openTrades.putSingle(trade, order.id)
+        openTrades.put(trade, order.id)
         matchOrder(order.id)
         return order
     }
@@ -103,26 +108,23 @@ class OrderManager(list: List<Order> = emptyList()) {
         }
 
         // get list of opposite trades
-        val matches: List<UUID> = openTrades[order1.trade.inverse]?.toList() ?: emptyList()
-        if (matches.isEmpty()) return false
+        val matches: List<UUID> = openTrades.getInverse(order1.trade)?.toList() ?: return false
 
         val id1: UUID = order1.id
-        val id2: UUID = matches.first()
+        val id2: UUID = matches.firstOrNull() ?: return false
         val order2 = orders[id2]
         if (!order2!!.stage.isPosted) {
-            openTrades.removeSingle(order2.trade, id2)
+            openTrades.remove(order2.trade, id2)
             warnInvalidState(String.format("Order %s was in openTrades when not posted", id2))
             return false
         }
 
-
-        // both orders should only be pointed to by openTrades if they are posted and yet to be matched or completed.
+        // Set both trades to matched and remove from open trades, so they aren't open for later matching
         orders[id1]!!.setMatched(id2)
         orders[id2]!!.setMatched(id1)
 
-        // remove trades from open trades so no one tries to match with these two orders.
-        openTrades.removeSingle(orders[id1]!!.trade, id1)
-        openTrades.removeSingle(orders[id2]!!.trade, id2)
+        openTrades.remove(orders[id1]!!.trade, id1)
+        openTrades.remove(orders[id2]!!.trade, id2)
 
         // Log change of Order state
         logMatch(order1.sellItem, order1.buyItem, id1, id2)
@@ -130,7 +132,7 @@ class OrderManager(list: List<Order> = emptyList()) {
     }
 
     // The implementations for hashCode and equals only care about the OrderMap
-    // The Trade multimap is only used for fast order matching
+    // The lookup table is only used for fast lookups
     override fun hashCode(): Int = orders.hashCode()
 
     override fun equals(other: Any?): Boolean {
